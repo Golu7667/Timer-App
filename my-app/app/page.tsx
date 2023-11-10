7efc0f3d35be8c2ed95d18aa31992a719815a9b0
unknown <goluchoudhary7808@gmail.com>
2023-11-10 15:37:53 +0530

    done project

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 
-const Home = () => {
+const Timer = () => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
 
@@ -10,7 +12,19 @@ const Home = () => {
 
     if (isActive) {
       interval = setInterval(() => {
-        setSeconds((prevSeconds) => prevSeconds + 1);
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 59) {
+            setMinutes((prevMinutes) => {
+              if (prevMinutes === 59) {
+                setHours((prevHours) => prevHours + 1);
+                return 0;
+              }
+              return prevMinutes + 1;
+            });
+            return 0;
+          }
+          return prevSeconds + 1;
+        });
       }, 1000);
     } else {
       clearInterval(interval);
@@ -28,18 +42,23 @@ const Home = () => {
   };
 
   const handleReset = () => {
+    setHours(0);
+    setMinutes(0);
     setSeconds(0);
     setIsActive(false);
   };
 
   return (
-    <div>
-      <div>Timer: {seconds} seconds</div>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
-      <button onClick={handleReset}>Reset</button>
+    <div className='w-100vw h-100vh flex justify-center items-center'>
+      <div className='w-96vw h-96vh grid justify-center items-center mt-44'>
+        <p className='w-96 flex justify-center mt-42 text-6xl font-extrabold font-mono text-slate-600'>Timer App</p>
+      <div className='w-96vw h-96vh flex justify-center mt-12 text-4xl font-extrabold'>{hours.toString().padStart(2, '0')}:{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</div>
+      <button onClick={handleStart} className='w-96 h-8 bg-cyan-400 rounded-lg mt-8 text-white font-extrabold'>Start</button>
+      <button onClick={handleStop} className='w-96 h-8 bg-cyan-400 rounded-lg mt-4 text-white font-extrabold'>Stop</button>
+      <button onClick={handleReset} className='w-96 h-8 bg-cyan-400 rounded-lg mt-4 text-white font-extrabold'>Reset</button>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default Timer;
